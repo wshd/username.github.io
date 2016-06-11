923a797c4059d6945fe6c852e685abe711ed911c
vitalii-kozachok <vitaliy.kozachok@solarwinds.com>
2016-06-11 16:42:07 +0300

    Csching requests

--- sw.js
+++ sw.js
@@ -2,6 +2,9 @@ var CACHE_NAME = 'ua-gb-cache-v1';
 var urlsToCache = [
     '/',
     '/assets/tpl/app.html',
+    'assets/tpl/blocks/aside.html',
+    'assets/tpl/blocks/header.html',
+    'assets/tpl/blocks/nav.html',
     'assets/tpl/order/list.html',
     'assets/tpl/order/history.html',
     'assets/tpl/client/list.html',
@@ -42,5 +45,41 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    //console.log(event.request);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the request. A request is a stream and
+                // can only be consumed once. Since we are consuming this
+                // once by cache and once by the browser for fetch, we need
+                // to clone the response
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'cors') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have 2 stream.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
 });
